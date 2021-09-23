88d4c73ae8a24e24c17b5ade502e696daf70b926
MahmoudElmahalawy <elmehalawy7@gmail.com>
2021-09-23 19:23:49 +0200

    Added temporary data for testing

---
+++ src/data/index.js
@@ -0,0 +1,168 @@
+export const quizzes = [
+	{
+		created: "2020-09-09 09:26:39",
+		description: "This is first quiz description",
+		id: 29,
+		modified: "2020-09-09 09:26:39",
+		questions_answers: [
+			{
+				answer_id: null,
+				answers: [
+					{
+						id: 122,
+						is_true: false,
+						text: "question 1 answer 1 false",
+					},
+					{
+						id: 123,
+						is_true: false,
+						text: "question 1 answer 2 false",
+					},
+					{
+						id: 124,
+						is_true: true,
+						text: "question 1 answer 3 true",
+					},
+					{
+						id: 125,
+						is_true: false,
+						text: "question 1 answer 4 false",
+					},
+				],
+				feedback_false: "question 1 false feedback",
+				feedback_true: "question 1 true feedback",
+				id: 53,
+				text: "question 1 text",
+			},
+			{
+				answer_id: null,
+				answers: [
+					{
+						id: 126,
+						is_true: true,
+						text: "question 2 answer 1 true",
+					},
+					{
+						id: 127,
+						is_true: false,
+						text: "question 2 answer 2 false",
+					},
+				],
+				feedback_false: "question 2 false feedback",
+				feedback_true: "question 2 true feedback",
+				id: 54,
+				text: "question 2 text",
+			},
+			{
+				answer_id: null,
+				answers: [
+					{
+						id: 128,
+						is_true: false,
+						text: "question 3 answer 1 false",
+					},
+					{
+						id: 129,
+						is_true: true,
+						text: "question 3 answer 2 true",
+					},
+					{
+						id: 130,
+						is_true: false,
+						text: "question 3 answer 3 false",
+					},
+				],
+				feedback_false: "question 3 false feedback",
+				feedback_true: "question 3 true feedback",
+				id: 55,
+				text: "question 3 text",
+			},
+		],
+		score: 2,
+		title: "First Quiz",
+		url: "https://www.youtube.com/watch?v=e6EGQFJLl04",
+	},
+	{
+		created: "2020-09-10 09:26:39",
+		description: "New Description",
+		id: 30,
+		modified: "2020-09-10 09:26:39",
+		questions_answers: [
+			{
+				answer_id: null,
+				answers: [
+					{
+						id: 1122,
+						is_true: false,
+						text: "question 1 answer 1 false",
+					},
+					{
+						id: 1123,
+						is_true: false,
+						text: "question 1 answer 2 false",
+					},
+					{
+						id: 1124,
+						is_true: true,
+						text: "question 1 answer 3 true",
+					},
+					{
+						id: 1125,
+						is_true: false,
+						text: "question 1 answer 4 false",
+					},
+				],
+				feedback_false: "question 1 false feedback",
+				feedback_true: "question 1 true feedback",
+				id: 153,
+				text: "question 1 text",
+			},
+			{
+				answer_id: null,
+				answers: [
+					{
+						id: 1126,
+						is_true: true,
+						text: "question 2 answer 1 true",
+					},
+					{
+						id: 1127,
+						is_true: false,
+						text: "question 2 answer 2 false",
+					},
+				],
+				feedback_false: "question 2 false feedback",
+				feedback_true: "question 2 true feedback",
+				id: 154,
+				text: "question 2 text",
+			},
+			{
+				answer_id: null,
+				answers: [
+					{
+						id: 1128,
+						is_true: false,
+						text: "question 3 answer 1 false",
+					},
+					{
+						id: 1129,
+						is_true: true,
+						text: "question 3 answer 2 true",
+					},
+					{
+						id: 1130,
+						is_true: false,
+						text: "question 3 answer 3 false",
+					},
+				],
+				feedback_false: "question 3 false feedback",
+				feedback_true: "question 3 true feedback",
+				id: 155,
+				text: "question 3 text",
+			},
+		],
+		score: null,
+		title: "quiz title",
+		url: "https://www.youtube.com/watch?v=e6EGQFJLl04",
+	},
+];
